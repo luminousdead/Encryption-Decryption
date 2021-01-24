44b31d52bafbfb14f2132cc1b1b6fe8a7dc60efa
Sergei_Gontarenko <solo.kutuzov@yandex.ru>
2021-01-24 15:38:57 +0300

    stage 5

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,69 @@
-import encryptor.Encryptor;
+import encryptor.EncryptorDecryptor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        int key = scanner.nextInt();
-        System.out.println(Encryptor.encrypt(input, key));
+        int key = 0;
+        String in = null, workStyle = "enc", out = null, data = null;
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-mode"))
+                workStyle = args[i+1];
+            if (args[i].equals("-key"))
+                key = Integer.parseInt(args[i+1]);
+            if (args[i].equals("-in"))
+                in = args[i+1];
+            if (args[i].equals("-out"))
+                out = args[i+1];
+            if (args[i].equals("-data"))
+                data = args[i+1];
+        }
+
+        String answer = "";
+        File file;
+        if (data != null) {
+            if (out == null) {
+                answer = EncryptorDecryptor.work(data, key, workStyle);
+                System.out.println(answer);
+            } else {
+                answer = EncryptorDecryptor.work(data, key, workStyle);
+                file = new File(out);
+                try (FileWriter writer = new FileWriter(file)) {
+                    writer.write(answer);
+                } catch (IOException e) {
+                    System.out.printf("An exception occurs %s", e.getMessage());
+                }
+            }
+        } else if (in != null) {
+            data = "";
+            file = new File(in);
+            try (Scanner scanner = new Scanner(file)) {
+                while (scanner.hasNext()) {
+                    data += scanner.nextLine();
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("No file found: " + in);
+            }
+
+            if (out == null) {
+                answer = EncryptorDecryptor.work(data, key, workStyle);
+                System.out.println(answer);
+            } else {
+                answer = EncryptorDecryptor.work(data, key, workStyle);
+                file = new File(out);
+                try (FileWriter writer = new FileWriter(file)) {
+                    writer.write(answer);
+                } catch (IOException e) {
+                    System.out.printf("An exception occurs %s", e.getMessage());
+                }
+            }
+        } else {
+            System.out.println(answer);
+        }
     }
 }
--- src/encryptor/EncryptorDecryptor.java
+++ src/encryptor/EncryptorDecryptor.java
@@ -4,20 +4,33 @@ public class EncryptorDecryptor {
     // abcdefghijklmnopqrstuvwxyz
     // A aB bC cD dE eF fG gH hI iJ jK kL lM mN nO oP pQ qR rS sT tU uV vW wX xY yZ z
     private static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase();
-    private static StringBuilder code = new StringBuilder(alphabet);
+//    private static StringBuilder code = new StringBuilder(alphabet);
 //    private static StringBuilder codeChangedByKey = new StringBuilder(alphabet);
 
-    public static String encrypt(String originalMsg, int key) {
-        String encryptedString = "";
-        String alphabetChangedByKey = alphabet.substring(key) + alphabet.substring(0, key);
-        for (int i = 0; i < originalMsg.length(); i++) {
-            if (alphabet.contains(String.valueOf(originalMsg.charAt(i)))) {
-                encryptedString += alphabetChangedByKey.charAt(alphabet.indexOf(String.valueOf(originalMsg.charAt(i))));
-            } else {
-                encryptedString += originalMsg.charAt(i);
-            }
-        }
+    public static String work(String originalMsg, int key, String workStyle) {
+        if (workStyle.equals("enc"))
+            return encrypt(originalMsg, key);
+        else if (workStyle.equals("dec"))
+            return decrypt(originalMsg, key);
+        else
+            return "Error!";
+    }
 
-        return encryptedString;
+    private static String encrypt (String originalMsg, int key) {
+        String output = "";
+        char[] msg = originalMsg.toCharArray();
+        for (int i = 0; i < originalMsg.length(); i++) {
+            output += Character.toString(msg[i] + key);
+        }
+        return output;
+    }
+
+    private static String decrypt (String originalMsg, int key) {
+        String output = "";
+        char[] msg = originalMsg.toCharArray();
+        for (int i = 0; i < originalMsg.length(); i++) {
+            output += Character.toString(msg[i] - key);
+        }
+        return output;
     }
 }
