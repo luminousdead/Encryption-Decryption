6622e73810017a7e3d1df3a0c3ebc05810280d68
Sergei_Gontarenko <solo.kutuzov@yandex.ru>
2021-01-24 11:30:08 +0300

    stage 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import encryptor.Encryptor;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String originalMessage = "we found a treasure!";
-        System.out.println(Encryptor.encrypt(originalMessage));
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int key = scanner.nextInt();
+        System.out.println(Encryptor.encrypt(input, key));
     }
 }
--- src/encryptor/Encryptor.java
+++ src/encryptor/Encryptor.java
@@ -1,20 +1,20 @@
 package encryptor;
 
 public class Encryptor {
+    // abcdefghijklmnopqrstuvwxyz
     // A aB bC cD dE eF fG gH hI iJ jK kL lM mN nO oP pQ qR rS sT tU uV vW wX xY yZ z
     private static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase();
     private static StringBuilder code = new StringBuilder(alphabet);
-    private static StringBuilder codeReverse = new StringBuilder(alphabet).reverse();
+//    private static StringBuilder codeChangedByKey = new StringBuilder(alphabet);
 
-    public static String encrypt(String originalMsg) {
+    public static String encrypt(String originalMsg, int key) {
         String encryptedString = "";
+        String alphabetChangedByKey = alphabet.substring(key) + alphabet.substring(0, key);
         for (int i = 0; i < originalMsg.length(); i++) {
-            if (originalMsg.charAt(i) == ' ') {
-                encryptedString += " ";
-            } else if (originalMsg.charAt(i) == '!') {
-                encryptedString += "!";
+            if (alphabet.contains(String.valueOf(originalMsg.charAt(i)))) {
+                encryptedString += alphabetChangedByKey.charAt(alphabet.indexOf(String.valueOf(originalMsg.charAt(i))));
             } else {
-                encryptedString += codeReverse.charAt(code.indexOf(String.valueOf(originalMsg.charAt(i))));
+                encryptedString += originalMsg.charAt(i);
             }
         }
 
